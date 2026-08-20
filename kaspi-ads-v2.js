77b8633ac122568721d52d626fdf3bab80703b8b
7masok <7masok@gmail.com>
2026-08-21 01:38:42 +0500

    Reload permanent stock alert dismiss behavior

--- kaspi-ads-v2.js
+++ kaspi-ads-v2.js
@@ -21,7 +21,7 @@
   document.head.appendChild(reservationCompat);
 
   const extra=document.createElement('script');
-  extra.src='./stock-alerts-rescue-v1.js?v=20260820-1732';
+  extra.src='./stock-alerts-rescue-v1.js?v=20260821-0140';
   extra.async=false;
   document.head.appendChild(extra);
 
