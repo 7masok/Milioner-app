e6fc3768eef29b261189e4cf1168bda75b034479
7masok <7masok@gmail.com>
2026-09-11 23:34:52 +0500

    Show Kaspi import errors inline

--- kaspi-ads-v2.js
+++ kaspi-ads-v2.js
@@ -21,7 +21,7 @@
   document.head.appendChild(kaspiStatusCompat);
 
   const ads=document.createElement('script');
-  ads.src='./kaspi-ads-v2-original.js?v=20260911-csv-paste';
+  ads.src='./kaspi-ads-v2-original.js?v=20260911-csv-paste2';
   ads.async=false;
   document.head.appendChild(ads);
 
