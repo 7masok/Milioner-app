cf980c4c3dc6cc9ee441e959468601ddd5cb64c0
7masok <7masok@gmail.com>
2026-09-25 01:14:54 +0500

    Fix order index migration newline

--- railway/migrations/130_order_lines_creation_date.sql
+++ railway/migrations/130_order_lines_creation_date.sql
@@ -1,1 +1,1 @@
-CREATE INDEX IF NOT EXISTS idx_order_lines_creation_date ON marketplace_order_lines(creation_date DESC);\n
+CREATE INDEX IF NOT EXISTS idx_order_lines_creation_date ON marketplace_order_lines(creation_date DESC);
